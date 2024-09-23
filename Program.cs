85978d8ba70af9e14d7e768289d587275d06fd89
wwwellen <ellen.http4@gmail.com>
2024-09-23 15:41:58 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program // classe principal do programa
+{
+    private static readonly Random globalRandom = new Random(); // criando um objeto random estático para gerar números aléatorios
+    static void Main (string[] args) // criando um método estático main, sendo ele o ponto de partida do programa
+    {
+        Jogo jogo = new Jogo(globalRandom); // criando uma instância do jogo que inicia ele
+        jogo.IniciarJogo();
+    }
+}
