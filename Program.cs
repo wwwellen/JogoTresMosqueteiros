0abbfb497862408dc61cd4f4585f40c1cb6b300d
wwwellen <ellen.http4@gmail.com>
2024-09-23 16:10:15 -0300

    criando a classe da roleta de resultados

--- Program.cs
+++ Program.cs
@@ -11,3 +11,22 @@ class Program // classe principal do programa
         jogo.IniciarJogo();
     }
 }
+
+class RoletaDeDuelo
+{
+    private Random aleatorio;
+    public RoletaDeDuelo(Random random) // construtor da RoletaDeDuelo
+    {
+        aleatorio = random;
+    }
+    public ResultadoDuelo Girar()
+    {
+        int resultado = aleatorio.Next(3); // 0, 1 ou 2
+        switch (resultado)
+        {
+            case 0: return ResultadoDuelo.Vitoria; // indica que o jogador venceu
+            case 1: return ResultadoDuelo.Empate; // indica que o jogo deu empate
+            default: return ResultadoDuelo.Derrota; // indica que o jogador perdeu
+        }
+    }
+}
